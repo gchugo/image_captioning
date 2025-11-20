0d9498aefc57da76a3d67234a381c30daa5d40d5
Hugo Gómez-Caraballo <146179137+gchugo@users.noreply.github.com>
2025-11-20 11:30:14 +0100

    Update train.py

--- utils/train.py
+++ utils/train.py
@@ -1,6 +1,15 @@
 import tensorflow as tf
-from models.decoder_transformer import create_padding_mask, create_look_ahead_mask
 
+def create_padding_mask(seq):
+    """Máscara para ignorar el padding (0)."""
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+def create_look_ahead_mask(size):
+    """Máscara triangular para ocultar palabras futuras."""
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask  # (seq_len, seq_len)
+    
 class Trainer:
     def __init__(self, encoder, decoder, optimizer):
         self.encoder = encoder
