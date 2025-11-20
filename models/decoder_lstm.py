16ecf76374fe3fecfac1e93341ef3d17a5c43828
Hugo Gómez-Caraballo <146179137+gchugo@users.noreply.github.com>
2025-11-20 11:44:51 +0100

    Add BahdanauAttention class to decoder LSTM

--- models/decoder_lstm.py
+++ models/decoder_lstm.py
@@ -28,44 +28,54 @@ class BahdanauAttention(tf.keras.Model):
 
         return context_vector, attention_weights
 
+import tensorflow as tf
+from tensorflow.keras import layers
+
+class BahdanauAttention(tf.keras.Model):
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = layers.Dense(units)
+        self.W2 = layers.Dense(units)
+        self.V = layers.Dense(1)
+
+    def call(self, features, hidden):
+        hidden_with_time_axis = tf.expand_dims(hidden, 1)
+        score = self.V(tf.nn.tanh(self.W1(features) + self.W2(hidden_with_time_axis)))
+        attention_weights = tf.nn.softmax(score, axis=1)
+        context_vector = attention_weights * features
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+        return context_vector, attention_weights
+
 class RNN_Decoder(tf.keras.Model):
     def __init__(self, embedding_dim, units, vocab_size):
         super(RNN_Decoder, self).__init__()
         self.units = units
-
-        # Embedding para las palabras de entrada (input al decoder)
         self.embedding = layers.Embedding(vocab_size, embedding_dim)
-        
-        # GRU (Gated Recurrent Unit) - versión simplificada de LSTM
         self.gru = layers.GRU(self.units,
                               return_sequences=True,
                               return_state=True,
                               recurrent_initializer='glorot_uniform')
-        
         self.fc1 = layers.Dense(self.units)
-        self.fc2 = layers.Dense(vocab_size) # Salida: probabilidad de cada palabra del vocabulario
-
+        self.fc2 = layers.Dense(vocab_size)
         self.attention = BahdanauAttention(self.units)
 
     def call(self, x, features, hidden):
-        # x: input actual (palabra anterior), shape (batch_size, 1)
-        
-        # 1. Atención: Mirar a la imagen basándose en el estado oculto anterior
+        # 1. Atención
         context_vector, attention_weights = self.attention(features, hidden)
 
-        # 2. Embedding de la palabra entrada
-        x = self.embedding(x) # (batch_size, 1, embedding_dim)
+        # 2. Embedding
+        x = self.embedding(x)
 
-        # 3. Concatenar vector de contexto (imagen) con el embedding (palabra)
+        # 3. Concatenar
         x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
 
-        # 4. Pasar por la GRU
-        output, state = self.gru(x)
+        # 4. GRU (CORREGIDO: Pasamos initial_state)
+        output, state = self.gru(x, initial_state=hidden)
 
-        # 5. Clasificación final
+        # 5. Clasificación
         x = self.fc1(output)
         x = tf.reshape(x, (-1, x.shape[2]))
-        x = self.fc2(x) # (batch_size, vocab_size)
+        x = self.fc2(x)
 
         return x, state, attention_weights
 
